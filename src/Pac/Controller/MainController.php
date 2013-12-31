3d6c98c0c1d2a160478d8b2cfd07761fe57c37b3
Denis Roussel <denis.roussel@gmail.com>
2013-12-31 20:22:58 +0100

    modification du front pour afficher les variations

--- src/Pac/Controller/MainController.php
+++ src/Pac/Controller/MainController.php
@@ -17,8 +17,13 @@ class MainController implements ControllerProviderInterface
         $controllers = $app['controllers_factory'];
 
         $controllers->match('/', function (Request $request, Application $app) {
+            $year = 2012;
+
             return $app['twig']->render('home.twig', array(
-                'biggestAmount' => SubventionPeer::retrieveBiggestAmountByYear(2012),
+                'year'                 => $year,
+                'biggestAmount'        => SubventionPeer::retrieveBiggestAmountByYear($year),
+                'biggestGrowthAmount'  => SubventionPeer::retrieveBiggestGrowthAmountByYear($year),
+                'biggestGrowthPercent' => SubventionPeer::retrieveBiggestGrowthPercentByYear($year),
             ));
         })
         ->bind('home');
@@ -28,10 +33,13 @@ class MainController implements ControllerProviderInterface
                 return $app->redirect($app['url_generator']->generate('by_zipcode', array('zipcode' => $request->request->get('zipcode'))));
             }
 
+            $year = 2012;
+
             return $app['twig']->render('zipcode.twig', array(
-                'zipcode'     => $zipcode,
-                'subventionsSum' => SubventionPeer::retrieveAmountSumByYearAndZipcode(2012, $zipcode, null),
-                'subventions' => SubventionPeer::retrieveAmountByYearAndZipcode(2012, $zipcode, null),
+                'year'           => $year,
+                'zipcode'        => $zipcode,
+                'subventionsSum' => SubventionPeer::retrieveAmountSumByYearAndZipcode($year, $zipcode, null),
+                'subventions'    => SubventionPeer::retrieveAmountByYearAndZipcode($year, $zipcode, null),
             ));
         })
         ->value('zipcode', null)
